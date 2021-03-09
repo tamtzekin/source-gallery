ea1065779b9f701ab55aefeb43df5a9979fd684a
root <root@LAPTOP-H86GJAI6.localdomain>
2021-03-09 11:24:37 +0000

    Added ActiveAdmin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
     # Users can sign up multiple time/make many users
     resources :users
 

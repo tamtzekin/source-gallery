edde813f015b4a9198a7222c94c90720238c7125
root <root@LAPTOP-H86GJAI6.localdomain>
2021-03-15 12:03:54 +0000

    Set up Categories model

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,20 @@ Rails.application.routes.draw do
   # ActiveAdmin URLs
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
+    # Categories controller
+    resources :categories
 
     # Items controller
     resources :items
-
+    
     # Users can sign up multiple time/make many users
     resources :users
-
+    
     # Singular - one session at a time
     resource :session
-
+    
+    
     get "about", to: "pages#about"
     
     root "pages#home"
